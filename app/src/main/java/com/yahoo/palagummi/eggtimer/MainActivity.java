22b55149252c88a9174f6b2e177e3e8177b0b303
Durga Sai Preetham <durgasaipreethamp@yahoo.com>
2016-12-21 17:10:38 -0800

    Complete working timer app

--- app/src/main/java/com/yahoo/palagummi/eggtimer/MainActivity.java
+++ app/src/main/java/com/yahoo/palagummi/eggtimer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.yahoo.palagummi.eggtimer;
 
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -13,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     TextView timerText = null;
     ImageView imageView = null;
     SeekBar seekBar = null;
+    boolean counterIsActive = false;
+    CountDownTimer countDownTimer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,20 +29,12 @@ public class MainActivity extends AppCompatActivity {
         seekBar.setMax(600);
         // initial setup for the seekBar and the timer
         seekBar.setProgress(30);
-        changeTimer(0,30);
+        updateTimer(30);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
-                int minutes = (int) progress / 60;
-                int seconds = progress - (minutes * 60);
-                if(progress < 10) {
-                    seekBar.setProgress(10);
-                    changeTimer(0,10);
-                }
-                else {
-                    changeTimer(minutes, seconds);
-                }
+                updateTimer(progress);
             }
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {}
@@ -47,8 +44,55 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void changeTimer(int minutes, int seconds) {
+    // helper method to change the timerText
+    public void updateTimer(int secondsLeft) {
         timerText = (TextView) findViewById(R.id.timerText);
-        timerText.setText(Integer.toString(minutes) + ":" + Integer.toString(seconds));
+        int minutes = (int) secondsLeft / 60;
+        int seconds = secondsLeft - (minutes * 60);
+        String minutesString = "";
+        String secondsString = "";
+        if (minutes < 10) {
+            minutesString = "0" + Integer.toString(minutes);
+        } else {
+            minutesString = Integer.toString(minutes);
+        }
+        if (seconds < 10) {
+            secondsString = "0" + Integer.toString(seconds);
+        } else {
+            secondsString = Integer.toString(seconds);
+        }
+        timerText.setText(minutesString+ ":" + secondsString);
+    }
+
+    public void controlTimer(View view) {
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        button = (Button) findViewById(R.id.button);
+        timerText = (TextView) findViewById(R.id.timerText);
+        counterIsActive = false;
+
+        if(seekBar.isEnabled() && !counterIsActive) {
+            seekBar.setEnabled(false);
+            button.setText("Reset");
+            counterIsActive = true;
+            countDownTimer = new CountDownTimer(seekBar.getProgress() * 1000 + 500, 1000) { // adding 1/2  of the timer to change the behaviour of long/1000 which is floored to int
+                @Override
+                public void onTick(long l) { // l is in milliseconds
+                    updateTimer((int) l / 1000);
+                }
+
+                @Override
+                public void onFinish() {
+                    timerText.setText("00:00");
+                    MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
+                    mPlayer.start();
+                }
+            }.start();
+        } else {
+            seekBar.setEnabled(true);
+            button.setText("Start");
+            timerText.setText("00:30");
+            seekBar.setProgress(30);
+            countDownTimer.cancel();
+        }
     }
 }
